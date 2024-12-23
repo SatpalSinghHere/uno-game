1b87854660c0640246d1dcd8152059c8d511d000
SatpalSinghHere <satpalsinghalive1@gmail.com>
2024-12-23 22:53:37 +0530

    `Refactored VisibleCards component: removed unused state, updated logic for discarding cards and determining whose turn it is`

--- components/VisibleCards.tsx
+++ components/VisibleCards.tsx
@@ -13,12 +13,9 @@ import { thisPlayerContext } from './PlayGround';
 
 const VisibleCards = ({ deck, myTurn }: { deck: Card[], myTurn: boolean }) => {
 
-  const [cards, setCards] = useState<Card[]>([])
-
   const SocketContext = useContext(socketContext)
   const ThisPlayerContext = useContext(thisPlayerContext)
-  const thisPLayerEmail = ThisPlayerContext.playerEmail
-
+  
   const gameState: GameState | null | undefined = SocketContext?.gameState
   const discardCard = gameState?.discardCard
 
@@ -26,14 +23,24 @@ const VisibleCards = ({ deck, myTurn }: { deck: Card[], myTurn: boolean }) => {
     if (discardCard?.color === cardObject.color || discardCard?.value === cardObject.value) {
 
       if (gameState?.players[0]) {
-        let players = gameState.players
-        // let thisPlayer = players.find(player => player?.email === ThisPlayerContext.playerEmail)
+        
         gameState.discardCard = cardObject
+        if(cardObject.value === 'R') gameState.clockwise = !gameState.clockwise
         if (gameState.clockwise) {
+          if(cardObject.value !== 'S'){
           gameState.whoseTurn = (gameState.whoseTurn as number + 1) % gameState.players.length
+          }
+          else{
+            gameState.whoseTurn = (gameState.whoseTurn as number + 2) % gameState.players.length
+          }
         }
         else {
+          if(cardObject.value !== 'S'){
           gameState.whoseTurn = (gameState.whoseTurn as number - 1 + gameState.players.length) % gameState.players.length
+          }
+          else{
+            gameState.whoseTurn = (gameState.whoseTurn as number - 2 + gameState.players.length) % gameState.players.length
+          }
         }
 
         gameState.players.forEach((player, index) => {
@@ -49,27 +56,12 @@ const VisibleCards = ({ deck, myTurn }: { deck: Card[], myTurn: boolean }) => {
 
   };
 
-  // const thisRef = useRef(null)
-
-  // if (myTurn && thisRef.current) {
-  //   thisRef.current.classList.add
-  // }
-
-
-
   return (
 
     <div className={'absolute bottom-[5%] h-36 left-1/2 translate-x-[-50%]'}>
 
       {deck && deck.map((cardObject, index) => {
         const translateX = (index + 1 - ((deck.length+4)/2)) * 28;
-
-
-        // const cardGap = divWidth / cards.length
-
-        // console.log(cardGap)
-
-        // const translateX = index * 28;
 
         let styles = {
           transform: `translateX(${translateX}px)`,
@@ -84,12 +76,6 @@ const VisibleCards = ({ deck, myTurn }: { deck: Card[], myTurn: boolean }) => {
             boxShadow: '0px -10px 10px blue',
             
         }}
-
-        // // Using clsx to conditionally build the class string dynamically
-        // const cardClasses = clsx(
-        //   'h-full w-auto absolute hover:translate-y-[-8px] hover:scale-125 hover:z-[100] duration-75 cursor-pointer bg-white rounded-lg',
-        //   // `translate-x-[${translateX}px]` // Tailwind-compatible class name with dynamic value
-        // );
 
         return (
           <div key={index} className={'h-full w-auto absolute rounded-lg hover:z-[100]'} style={styles} onClick={() => { if(myTurn)useCard(cardObject) }}>
