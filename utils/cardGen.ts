97e96c67f9b3873194fd1975596cc9b4a0f641b4
SatpalSinghHere <satpalsinghalive1@gmail.com>
2024-12-24 22:12:42 +0530

    `Added id property to Card objects and modified randomDeckGen function to include id property in generated deck`

--- utils/cardGen.ts
+++ utils/cardGen.ts
@@ -1,13 +1,14 @@
 import { Card, cardList } from "./cardObjects"
 
 export const randomDeckGen = (length: number) => {
-    const randomDeck = []
-    for (let i = 0; i < length; i++) {
-      const index = Math.floor(Math.random() * cardList.length)
-      randomDeck.push(cardList[index])
-    }
-    return randomDeck
+  const randomDeck = []
+  for (let i = 0; i < length; i++) {
+    const index = Math.floor(Math.random() * cardList.length)
+    let card = {...cardList[index], id: crypto.randomUUID().replace(/-/g, '').slice(0, 10)}
+    randomDeck.push(cardList[index])
   }
+  return randomDeck
+}
   
 export const sortCards = (deck : Card[]) => {
     return deck.sort((a, b) => {
