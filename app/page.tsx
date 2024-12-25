9d10b148439e24ee170d0dd81b53ae1db38422d9
SatpalSinghHere <satpalsinghalive1@gmail.com>
2024-12-25 18:54:19 +0530

    `Added flipped property to Card objects and updated layout and styles for card flipping functionality`

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Navbar from "@/components/Navbar";
 import CardTemplate from "@/utils/Card";
 import CardBack from "@/utils/CardBack";
 import { useRef, useState } from "react";
+import { motion } from "framer-motion";
 
 export default function Home() {
   const cardRef = useRef(null)
@@ -16,11 +17,14 @@ export default function Home() {
     transform: 'rotateY(180deg) translate(27%, 0)',
   }
 
-  return (
+  return cardFlipped?(
     <div className="dark w-full h-[100vh] bg-slate-800">
 
       <Navbar />
-      <div onClick={flipCard} className={cardFlipped?"transform flip-y left-1/2 flex relative w-28 h-36 duration-100":"h-36 w-28 left-1/2 flex relative duration-100"} >
+      <button onClick={flipCard}>FLIP CARD</button>
+      <motion.div 
+      layoutId="abc"
+      onClick={flipCard} className={cardFlipped?"transform flip-y fixed left-1/4 top-1/4 flex w-28 h-36 duration-100":"h-36 w-28 left-1/2 flex relative duration-100"} >
         <div onClick={flipCard} className={cardFlipped?"absolute h-36":"z-10 absolute h-36"}>
           <CardTemplate className="h-36 w-auto bg-white rounded-lg" color='blue' value='9' />
         </div>
@@ -28,8 +32,28 @@ export default function Home() {
         <CardBack className={"transform flip-y w-auto z-1 h-full bg-white rounded-lg duration-500"} />
         </div>
         
-      </div>
+      </motion.div>
+
+    </div>
+  ):
+  (
+    <div className="dark w-full h-[100vh] bg-slate-800">
+
+      <Navbar />
+      <button onClick={flipCard}>FLIP CARD</button>
+      <motion.div 
+      layoutId="abc"
+      onClick={flipCard} className={cardFlipped?"transform flip-y fixed left-3/4 top-3/4 flex w-28 h-36 duration-100":"h-36 w-28 fixed left-3/4 top-3/4 flex duration-100"} >
+        <div onClick={flipCard} className={cardFlipped?"absolute h-36":"z-10 absolute h-36"}>
+          <CardTemplate className="h-36 w-auto bg-white rounded-lg" color='blue' value='9' />
+        </div>
+        <div onClick={flipCard} className={"absolute h-36"}>
+        <CardBack className={"transform flip-y w-auto z-1 h-full bg-white rounded-lg duration-500"} />
+        </div>
+        
+      </motion.div>
 
     </div>
   )
+  
 }
