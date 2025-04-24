3142e4820db7d388a40e54bce76b4417a5926937
SatpalSinghHere <satpalsinghalive1@gmail.com>
2025-04-24 09:42:58 +0530

    added sidebar for chatbox

--- app/game/layout.tsx
+++ app/game/layout.tsx
@@ -1,11 +1,20 @@
 import React, { ReactNode } from 'react'
 import SocketProvider from '../context/SocketProvider'
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+
 
 const layout = ({ children }: { children: ReactNode }) => {
     return (
         <>
             <SocketProvider>
-                {children}
+                <SidebarProvider>
+                    <AppSidebar />
+                    <main>
+                        <SidebarTrigger />
+                        {children}
+                    </main>
+                </SidebarProvider>
             </SocketProvider>
         </>
     )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,35 +1,96 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      boxShadow: {
-        textoutlinewhite: "0px 0px 0px 2px white",
-        textoutlineblack: "0px 0px 0px 2px black",
-      },
-      keyframes:{
-        fadeup: {
-          "0%": { opacity: '1', transform: "translate(-30%,-30%)" },
-          "30%": { opacity: '1', transform: "translate(-30%,-30%)" },
-          // "30%": { opacity: '1', transform: "translate(0,0)" },
-          "100%": { opacity: '0', transform: "translate(-30%,-100%)" },
-        }
-      },
-      animation: {
-        fadeup: "fadeup 4s ease-out",
-      },
-    },
-    
+  	extend: {
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			},
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
+  			}
+  		},
+  		boxShadow: {
+  			textoutlinewhite: '0px 0px 0px 2px white',
+  			textoutlineblack: '0px 0px 0px 2px black'
+  		},
+  		keyframes: {
+  			fadeup: {
+  				'0%': {
+  					opacity: '1',
+  					transform: 'translate(-30%,-30%)'
+  				},
+  				'30%': {
+  					opacity: '1',
+  					transform: 'translate(-30%,-30%)'
+  				},
+  				'100%': {
+  					opacity: '0',
+  					transform: 'translate(-30%,-100%)'
+  				}
+  			}
+  		},
+  		animation: {
+  			fadeup: 'fadeup 4s ease-out'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [require("@xpd/tailwind-3dtransforms")],
+  plugins: [require("@xpd/tailwind-3dtransforms"), require("tailwindcss-animate")],
 };
 export default config;
