c62537d8022e91b2609c6e9a90f8f79ac576c067
SatpalSinghHere <satpalsinghalive1@gmail.com>
2025-04-25 09:43:53 +0530

    remove sidebar component

--- app/game/layout.tsx
+++ app/game/layout.tsx
@@ -1,20 +1,11 @@
 import React, { ReactNode } from 'react'
 import SocketProvider from '../context/SocketProvider'
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/app-sidebar"
-
 
 const layout = ({ children }: { children: ReactNode }) => {
     return (
         <>
             <SocketProvider>
-                <SidebarProvider>
-                    <AppSidebar />
-                    <main>
-                        <SidebarTrigger />
-                        {children}
-                    </main>
-                </SidebarProvider>
+                {children}
             </SocketProvider>
         </>
     )
